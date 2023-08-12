da367909bc13f2148486de6da801823ebb98e01c
PyotSawe <potsaw01@gmail.com>
2023-08-12 02:09:15 +0000

    Implemented a calculator with all operators

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+if __name__ == '__main__':
     from calculator_1 import add, sub, mul, div
     from sys import argv
+    argc = len(argv)
+    ops = ['+', '-', '*', '/']
 
-    if len(argv) != 4:
+    if argc != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    numb1 = int(argv[1])
-    numb2 = int(argv[3])
-
-    if argv[2] not in ["+", "-", "*", "/"]:
+    op = argv[2]
+    a = int(argv[1])
+    b = int(argv[3])
+    if op not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    elif argv[2] is "+":
-        print("{} + {} = {}".format(numb1, numb2, add(numb1, numb2)))
-    elif argv[2] is "-":
-        print("{} - {} = {}".format(numb1, numb2, sub(numb1, numb2)))
-    elif argv[2] is "*":
-        print("{} * {} = {}".format(numb1, numb2, mul(numb1, numb2)))
-    elif argv[2] is "/":
-        print("{} / {} = {}".format(numb1, numb2, div(numb1, numb2)))
+    if op == '+':
+        print("{:d} {} {:d} = {:d}".format(a, op, b, add(a, b)))
+    if op == '-':
+        print("{:d} {} {:d} = {:d}".format(a, op, b, sub(a, b)))
+    if op == '*':
+        print("{:d} {} {:d} = {:d}".format(a, op, b, mul(a, b)))
+    if op == '/':
+        print("{:d} {} {:d} = {:d}".format(a, op, b, div(a, b)))
