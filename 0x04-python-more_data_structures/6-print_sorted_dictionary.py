b174929db5209c743e65c56ca778e7a394faafc6
PyotSawe <potsaw01@gmail.com>
2023-08-16 23:21:07 +0300

    Used sorted builtin to process the obj

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted_dict = sorted(a_dictionary.items())
+    sorted_dict = sorted(a_dictionary)
     
-    for k, v in sorted_dict:
-        print('{0}: {1}'.format(k, v))
+    for v in sorted_dict:
+        print('{}: {}'.format(v, a_dictionary[values]))
