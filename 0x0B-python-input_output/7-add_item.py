054d4ce9ac1bffb19843501355aff6c2f57259bc
PyotSawe <potsaw01@gmail.com>
2023-09-13 20:23:03 +0300

    Fixed Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,8 +2,8 @@
 
 """Saves All CMD_ARGS to file"""
 from sys import argv
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 if __name__ == "__main__":
     filename = "add_item.json"
     try:
