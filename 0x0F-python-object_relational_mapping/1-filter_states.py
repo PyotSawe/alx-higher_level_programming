739c4457e616e71a7d4c6b8442c005a0e81f1b50
PyotSawe <potsaw01@gmail.com>
2023-11-24 20:56:06 +0300

    fixed 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
-'''
-Lists all states with a name starting with N
-'''
-
-import MySQLdb
 from sys import argv
+import MySQLdb
 
-if __name__ == "__main__":
 
-        conn = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-        query_rows = cur.fetchall()
-        for row in query_rows:
-                if row[1].startswith('N'):
-                        print(row)
-        cur.close()
-        conn.close()
+def connectDB():
+    """
+    Connect to database and quering
+    """
+    try:
+        db_connection = MySQLdb.connect(host="localhost", port=3306,
+                                        user=argv[1], password=argv[2],
+                                        db=argv[3], charset="utf8")
+    except Exception:
+        print("Can't connect to database")
+        return 0
+    cur = db_connection.cursor()
+    sql = "SELECT id, name FROM states ORDER BY id ASC;"
+    cur.execute(sql)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if 'N' == row[1][0]:
+            print(row)
+    cur.close()
+    db_connection.close()
+
+
+connectDB()
