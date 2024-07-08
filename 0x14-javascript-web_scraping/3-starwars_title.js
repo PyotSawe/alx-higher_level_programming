fa5ef988ec878ef492869e0ee2e01f64e996aebc
Zhor P. Saweh <123611421+PyotSawe@users.noreply.github.com>
2024-07-08 08:33:42 +0000

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const request = require('request');
-const ID = process.argv[2];
-const endPoint = `https://swapi-api.alx-tools.com/api/films/${ID}`;
-
-request(endPoint, function (_err, _res, body) {
-  body = JSON.parse(body);
-  console.log(body.title);
-});
